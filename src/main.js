8b496adf5f4e8c22730518af00ed9f47bc667890
pavelhladki <phladki@objectstyle.com>
2018-06-12 17:07:39 +0300

    initial commit

---
+++ src/main.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+class Main extends React.Component {
+  render() {
+    return (
+      <View>
+        <Text>Hi, Pavel! Congratulation! Your First React Native App!</Text>
+        <Text>NEW TEXT!!!</Text>
+        <Text>Shake your phone to open the developer menu.</Text>
+      </View>
+    );
+  }
+}
+
+export default Main;
