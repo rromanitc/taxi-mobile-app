57fb94d257a8f681db1c569ad41b1b9cbe92cb86
pavelhladki <phladki@objectstyle.com>
2018-06-12 13:09:06 +0300

    initial commit

--- App.js
+++ App.js
@@ -5,7 +5,9 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <Text>Hi, Pavel! Congratulation! Your First React Native App!
+
+        Open up App.js to start working on your app!</Text>
         <Text>Changes you make will automatically reload.</Text>
         <Text>Shake your phone to open the developer menu.</Text>
       </View>
